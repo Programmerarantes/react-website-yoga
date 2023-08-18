6746e64020f4d56b747aa27876e41f4c4e7b3c42
Matheus Arantes <125316650+Programmerarantes@users.noreply.github.com>
2023-08-18 11:51:18 -0300

    fixing navmenubar

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,22 @@ import React from 'react'
 function Home() {
   return (
     <div className='home'>
-       
+       <section id="container-one" class="container-one">
+            <div class="box-container-one">
+                <div class="intro">
+                    <span class="separator">-
+                        <span class="legend">Yoga para Gestantes e Parto Ativo</span>
+                    </span>
+                    <div class="text-one">
+                        <h3>Um momento especial de conexão com o seu corpo e com o seu bebê.</h3>
+                        <p>Aulas de yoga pré-natal preparatórias para o parto e pós parto, conduzidas por uma professora e doula especializada!</p>
+                    </div>
+                </div>
+                <div class="btn-container">
+                    <a class="btn-one" href="#container-four">CONHEÇA OS BENEFÍCIOS</a>
+                </div>
+            </div>
+        </section>
     </div>
   )
 }
